12bf035ebae28f24cfbb78e5e3c2980f911779cc
albin <albinpkr96@gmail.com>
2018-10-19 22:00:29 +0530

    firebase setup

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyChg60s1BcbXk2aFNL6J86KrWt5IjYWhtU",
+    authDomain: "spot-that-fire-nsac.firebaseapp.com",
+    databaseURL: "https://spot-that-fire-nsac.firebaseio.com",
+    projectId: "spot-that-fire-nsac",
+    storageBucket: "spot-that-fire-nsac.appspot.com",
+    messagingSenderId: "1074092663397"
+};
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -2,10 +2,28 @@ const express = require('express');
 const app = express();
 const debug = require('debug')('index');
 const router = express.Router();
+const firebase = require('firebase');
+const firebaseConfig = require('../../firebaseConfig.js');
 
-router.get('/',(req,res,next) => {
-    res.send("Welcome to RPMCARE-APIS!");
+firebase.initializeApp(firebaseConfig);
+
+
+router.post('/signUp',(req,res,next) => {
+  firebase.database().ref('users/'+ req.body.mobileNo).push({
+    ssn: req.body.ssn,
+  });
+
 });
+
+router.get('/fireLoc',(req,res,next) => {
+  firebase.database().ref('fire_loc/'+ req.body.lat + req.body.long).push({
+    lat: req.body.lat,
+    long :req.body.long,
+
+  });
+
+});
+
 
 
 module.exports = router;

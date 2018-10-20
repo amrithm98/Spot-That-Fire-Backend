7b985270ad70408c4c1021d1e9f898cddf1dbed1
amrithm98 <amrithchallenger@gmail.com>
2018-10-20 12:46:00 +0530

    Small Fix Spell

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -6,24 +6,30 @@ const Sendotp = require('sendotp')
 const sendOtp = new Sendotp('243627AqoWc8HWwq5bcab24a')
 
 router.get('/',(req,res,next) => {
-    res.send("Welcome to RPMCARE-AUTH!");
+    res.send("Welcome to SPOT FIRE-AUTH!");
 });
+
 router.post('/getOTP',(req,res,next)=>{
     const mobileNo = req.body.mobileNo
     const senderId = "FAlert"
     const otp = Math.floor(1000 + Math.random() * 9000);
     sendOtp.send(mobileNo, senderId,otp, function (error, data) {
       console.log(data);
-});
+      if(!error)    
+        res.json({"success":true});
+        else
+        res.json({"success":false});
+    });
 })
+
 router.post('/verifyOTP',(req,res,next)=>{
     const mobileNo = req.body.mobileNo
     const otp = req.body.otp
     sendOtp.verify(mobileNo, otp, function (error, data) {
  // data object with keys 'message' and 'type'
-        if(data.type == 'success') res.json({"Success":true})
-        if(data.type == 'error') res.json({"Success":false   })
-});
+        if(data.type == 'success') res.json({"success":true})
+        if(data.type == 'error') res.json({"success":false   })
+    });
 })
 
 module.exports = router;
